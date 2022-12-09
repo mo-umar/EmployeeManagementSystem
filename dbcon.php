bc2c523e75e0af35589f3c88a11b7db82a26edb8
Mohammad Umar <60454035+mo-umar@users.noreply.github.com>
2022-12-09 00:05:01 -0500

    Update dbcon.php

--- dbcon.php
+++ dbcon.php
@@ -1,20 +1,20 @@
 <?php
-
+/*
 // Local Connection
 $con = mysqli_connect("localhost","root","","emsdb");
 
 if(!$con){
     die('Connection Failed'. mysqli_connect_error());
 }
+*/
 
 
-/*
 // Remote Connection AWS
 $con = mysqli_connect("localhost","admin","1234567890","emsdb");
 
 if(!$con){
     die('Connection Failed'. mysqli_connect_error());
 }
-*/
+
 
 ?>
